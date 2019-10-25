0475b1f998eff01b21f7d2b66e8bd07d3d5ea42f
Budafly <info@budafly.com>
2019-10-24 19:25:12 -0500

    Update OverAppsService.java

--- src/android/Services/OverAppsService.java
+++ src/android/Services/OverAppsService.java
@@ -34,7 +34,7 @@ import org.apache.cordova.overApps.GeneralUtils.KeyDispatchLayout;
 
 
 import java.util.Date;
-import org.apache.cordova.overApps.DrawOverApps.R;
+import com.adobe.phonegap.appbuda.R;
 /**
  * Created by Mohamed Sayed .
  */
